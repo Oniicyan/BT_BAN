b4e8b791e84cbc1a2262c8e555f724115be98fd0
泥浆 <8310999+oniicyan@user.noreply.gitee.com>
2024-07-29 05:49:41 +0000

    update BT_BAN_SETUP.ps1.
    Signed-off-by: 泥浆 <8310999+oniicyan@user.noreply.gitee.com>

--- BT_BAN_SETUP.ps1
+++ BT_BAN_SETUP.ps1
@@ -42,6 +42,7 @@ if (Get-ScheduledTask BT_BAN_$BTNAME -ErrorAction Ignore) {
 	cls
 	echo ""
 	echo "  BT_BAN_$BTNAME 任务计划已存在"
+	echo ""
 	echo "  同名客户端多开，即使目录不同，也需修改文件名以区分"
 	echo ""
 	echo "  覆盖请按 Enter 键，退出请按 Ctrl + C 键"
