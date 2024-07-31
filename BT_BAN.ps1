b259751b279a5f7c27db5cc75f68ba3a4fc384ba
Oniicyan <130678414+Oniicyan@users.noreply.github.com>
2024-08-01 02:30:28 +0800

    Update BT_BAN.ps1

--- BT_BAN.ps1
+++ BT_BAN.ps1
@@ -1,67 +1,79 @@
+$PS1URL = 'https://bt-ban.pages.dev/BT_BAN.ps1'
 $ZIPURL = 'https://bt-ban.pages.dev/IPLIST.zip'
-$IRMURL = 'https://bt-ban.pages.dev/BT_BAN.ps1'
 
-$BTPATH = $args[0]
-$BTNAME = [System.IO.Path]::GetFileName($BTPATH)
+$TOAST ={
+	$XML = '<toast DDPARM><visual><binding template="ToastText01"><text id="1">DDTEXT</text></binding></visual><audio silent="BOOL"/></toast>'
+	$XmlDocument = [Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime]::New()
+	$XmlDocument.loadXml($XML.Replace("DDPARM","$DDPARM").Replace("DDTEXT","$DDTEXT").Replace("BOOL","$SILENT"))
+	$AppId = '{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe'
+	[Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime]::CreateToastNotifier($AppId).Show($XmlDocument)
+}
+
+if (! (Get-NetFirewallRule -DisplayName "BT_BAN_*")) {
+	$DDPARM = 'scenario="incomingCall"'
+	$DDTEXT = "过滤规则丢失，请重新执行`niex (irm bt-ban.pages.dev)"
+	$SILENT = 'false'
+
+	&$TOAST
+	exit 1
+}
+
+New-Item -ItemType Directory -Path $env:USERPROFILE\BT_BAN -Force | Out-Null
+while ($ZIP -lt 5) {
+	try {
+		Invoke-WebRequest -OutFile $env:USERPROFILE\BT_BAN\IPLIST.zip $ZIPURL -TimeoutSec 30
+		break
+	} catch {
+		sleep 60
+		$ZIP++
+		if ($ZIP -ge 5) {exit 1}
+	}
+}
+Expand-Archive -Force -Path $env:USERPROFILE\BT_BAN\IPLIST.zip -DestinationPath $env:USERPROFILE\BT_BAN
+$IPLIST = Get-Content $env:USERPROFILE\BT_BAN\IPLIST.txt
+
 $DYKWID = '{3817fa89-3f21-49ca-a4a4-80541ddf7465}'
-
-Invoke-WebRequest -OutFile "$env:temp\BT_BAN_IPLIST.zip" $ZIPURL -TimeoutSec 30
-Expand-Archive -Force -Path $env:temp\BT_BAN_IPLIST.zip -DestinationPath $env:temp\BT_BAN
-$IPLIST = Get-Content $env:temp\BT_BAN\IPLIST.txt
-
-$RULES = Get-NetFirewallRule -DisplayName "BT_BAN_$BTNAME" -ErrorAction Ignore
-
-$SET_RULES = {
-	Remove-NetFirewallRule -DisplayName "BT_BAN_$BTNAME" -ErrorAction Ignore
-	New-NetFirewallRule -DisplayName "BT_BAN_$BTNAME" -Direction Inbound -Action Block -Program $BTPATH -RemoteDynamicKeywordAddresses $DYKWID | Out-Null
-	New-NetFirewallRule -DisplayName "BT_BAN_$BTNAME" -Direction Outbound -Action Block -Program $BTPATH -RemoteDynamicKeywordAddresses $DYKWID | Out-Null
-}
-
-if (($RULES | Out-String -Stream | Select-String -SimpleMatch $DYKWID).Count -ne 2) {
-	&$SET_RULES
-} elseif (($RULES | Get-NetFirewallApplicationFilter | Out-String -Stream | Select-String -SimpleMatch $BTPATH).Count -ne 2) {
-	&$SET_RULES
-} elseif (($RULES | Out-String -Stream | Select-String -SimpleMatch Inbound).Count -ne 1) {
-	&$SET_RULES
-}
-
 if (Get-NetFirewallDynamicKeywordAddress -Id $DYKWID -ErrorAction Ignore) {
 	Update-NetFirewallDynamicKeywordAddress -Id $DYKWID -Addresses $IPLIST | Out-Null
-	$DDTIME = 'short'
+	$DDPARM = ''
 	$DDTEXT = 'BT_BAN_IPLIST 已更新'
 	$SILENT = 'true'
 } else {
 	New-NetFirewallDynamicKeywordAddress -Id $DYKWID -Keyword "BT_BAN_IPLIST" -Addresses $IPLIST | Out-Null
-	$DDTIME = 'long'
+	$DDPARM = 'duration="long"'
 	$DDTEXT = 'BT_BAN_IPLIST 已启用'
 	$SILENT = 'false'
 }
 
-[XML]$TASKINFO = Export-ScheduledTask BT_BAN_$BTNAME -ErrorAction Ignore
-if (-Not ($TASKINFO.Task.Actions.Exec.Command -Match "BT_BAN_$BTNAME.vbs")) {
+$SET_UPDATE ={
 	$VBS = 'createobject("wscript.shell").run "CMD",0'
 	$CMD = "powershell `"`"iex `"`"`"`"&{`$(irm $IRMURL -TimeoutSec 30)} '$BTPATH'`"`"`"`"`"`""
-	$VBS.Replace("CMD","$CMD") >$env:USERPROFILE\BT_BAN_$BTNAME.vbs
-
-	Unregister-ScheduledTask BT_BAN_$BTNAME -Confirm:$false -ErrorAction Ignore
-
+	$VBS.Replace("CMD","$CMD") >$env:USERPROFILE\BT_BAN\UPDATE.vbs
+	
 	$PRINCIPAL = New-ScheduledTaskPrincipal -UserId (whoami) -RunLevel Highest
-	$SETTINGS = New-ScheduledTaskSettingsSet -RestartCount 5 -RestartInterval (New-TimeSpan -Seconds 60) -StartWhenAvailable -AllowStartIfOnBatteries
-	$TRIGGER = New-ScheduledTaskTrigger -Once -At 00:00 -RepetitionInterval  (New-TimeSpan -Hours 8)
-	$ACTION = New-ScheduledTaskAction -Execute $env:USERPROFILE\BT_BAN_$BTNAME.vbs
+	$SETTINGS = New-ScheduledTaskSettingsSet -RestartCount 5 -RestartInterval (New-TimeSpan -Seconds 60) -AllowStartIfOnBatteries
+	$TRIGGER = New-ScheduledTaskTrigger -Once -At 00:05 -RepetitionInterval  (New-TimeSpan -Hours 8)
+	$ACTION = New-ScheduledTaskAction -Execute $env:USERPROFILE\BT_BAN\UPDATE.vbs
 	$TASK = New-ScheduledTask -Principal $PRINCIPAL -Settings $SETTINGS -Trigger $TRIGGER -Action $ACTION
 
-	Register-ScheduledTask BT_BAN_$BTNAME -InputObject $TASK | Out-Null
-	Start-ScheduledTask BT_BAN_$BTNAME
-	
-	$DDTIME = 'short'
+	$TASKLIST = (Get-ScheduledTask BT_BAN_*).TaskName
+	if ($TASKLIST) {Unregister-ScheduledTask $TASKLIST -Confirm:$false}
+	Register-ScheduledTask BT_BAN_UPDATE -InputObject $TASK | Out-Null
+
+	$DDPARM = 'duration="long"'
 	$DDTEXT = (echo "$DDTEXT / 任务计划已重建")
 	$SILENT = 'false'
+
+	# 删除旧版本文件，此部分保留一段时间
+	$SYSTMP = [System.Environment]::GetEnvironmentVariable('TEMP','Machine')
+	$SYSUSR = 'C:\Windows\system32\config\systemprofile'
+	Remove-Item $SYSTMP -Include BT_BAN* -Recurse -Force -ErrorAction Ignore
+	Remove-Item $SYSUSR -Include BT_BAN* -Recurse -Force -ErrorAction Ignore
 }
 
+[XML]$TASKINFO = Export-ScheduledTask BT_BAN_UPDATE -ErrorAction Ignore
+if (! ($TASKINFO.Task.RegistrationInfo.URI -Match 'BT_BAN_UPDATE')) {
+	&$SET_UPDATE
+}
 
-$XML = '<toast duration="DDTIME"><visual><binding template="ToastText01"><text id="1">DDTEXT</text></binding></visual><audio silent="BOOL"/></toast>'
-$XmlDocument = [Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime]::New()
-$XmlDocument.loadXml($XML.Replace("DDTIME","$DDTIME").Replace("DDTEXT","$DDTEXT").Replace("BOOL","$SILENT"))
-$AppId = '{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe'
-[Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime]::CreateToastNotifier($AppId).Show($XmlDocument)
+&$TOAST
