e14501993367c4e7b31b999688a00174fb210a61
Oniicyan <130678414+Oniicyan@users.noreply.github.com>
2024-08-02 15:55:59 +0800

    Update BT_BAN_UNSET.ps1

--- BT_BAN_UNSET.ps1
+++ BT_BAN_UNSET.ps1
@@ -6,20 +6,6 @@ if ((Fltmc).Count -eq 3) {
 	exit
 }
 
-$TASKLIST = (Get-ScheduledTask BT_BAN_*).TaskName
-if ($TASKLIST) {
-	echo ""
-	echo "  删除以下任务计划"
-	echo ""
-	$TASKLIST | ForEach-Object {'  ' + $_}
-	echo ""
-	pause
-	Unregister-ScheduledTask $TASKLIST -Confirm:$false
-} else {
-	echo ""
-	echo "  没有需要删除的任务计划"
-}
-
 $RULELIST = Get-NetFirewallRule -DisplayName BT_BAN_* | Select-Object -Property Displayname, Direction
 if ($RULELIST) {
 	echo ""
@@ -34,19 +20,18 @@ if ($RULELIST) {
 	echo "  没有需要删除的过滤规则"
 }
 
-$FILELIST = (Get-Childitem $env:USERPROFILE\BT_BAN -Recurse).FullName
-if ($FILELIST) {
+$TASKLIST = (Get-ScheduledTask BT_BAN_*).TaskName
+if ($TASKLIST) {
 	echo ""
-	echo "  删除以下脚本文件"
+	echo "  删除以下任务计划"
 	echo ""
-	echo "  $env:USERPROFILE\BT_BAN"
-	$FILELIST | ForEach-Object {'  ' + $_}
+	$TASKLIST | ForEach-Object {'  ' + $_}
 	echo ""
 	pause
-	Remove-Item $env:USERPROFILE\BT_BAN -Force -Recurse -ErrorAction Ignore
+	Unregister-ScheduledTask $TASKLIST -Confirm:$false
 } else {
 	echo ""
-	echo "  没有需要删除的脚本文件"
+	echo "  没有需要删除的任务计划"
 }
 
 $DYKWID = '{3817fa89-3f21-49ca-a4a4-80541ddf7465}'
@@ -62,6 +47,21 @@ if ($DYKWNAME) {
 } else {
 	echo ""
 	echo "  没有需要删除的动态关键字"
+}
+
+$FILELIST = (Get-Childitem $env:USERPROFILE\BT_BAN -Recurse).FullName
+if ($FILELIST) {
+	echo ""
+	echo "  删除以下脚本文件"
+	echo ""
+	echo "  $env:USERPROFILE\BT_BAN"
+	$FILELIST | ForEach-Object {'  ' + $_}
+	echo ""
+	pause
+	Remove-Item $env:USERPROFILE\BT_BAN -Force -Recurse -ErrorAction Ignore
+} else {
+	echo ""
+	echo "  没有需要删除的脚本文件"
 }
 
 echo ""
