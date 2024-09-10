24e78cf0044a4b95ee0a8a3cda06af39eb11527f
Oniicyan <130678414+Oniicyan@users.noreply.github.com>
2024-09-10 16:58:08 +0800

    Update BT_BAN_UNSET.ps1

--- BT_BAN_UNSET.ps1
+++ BT_BAN_UNSET.ps1
@@ -1,69 +1,50 @@
+Remove-Variable * -ErrorAction Ignore
+
 if ((Fltmc).Count -eq 3) {
-	echo ""
-	echo "  请以管理员权限重新执行"
-	echo ""
-	pause
-	exit
+	Write-Host "`n  请以管理员权限执行`n"
+	return
 }
 
-$RULELIST = Get-NetFirewallRule -DisplayName BT_BAN_* | Select-Object -Property Displayname, Direction
-if ($RULELIST) {
-	echo ""
-	echo "  清除以下过滤规则"
-	echo ""
+if ($RULELIST = Get-NetFirewallRule -DisplayName BT_BAN_*) {
+	Write-Host "`n  清除以下过滤规则`n"
 	$RULELIST | ForEach-Object {'  ' + $_.DisplayName + ' (' + $_.Direction + ')'}
-	echo ""
+	Write-Host
 	pause
-	Remove-NetFirewallRule -DisplayName $RULELIST.DisplayName
+	Remove-NetFirewallRule $RULELIST
 } else {
-	echo ""
-	echo "  没有需要清除的过滤规则"
+	Write-Host "`n  没有需要清除的过滤规则`n"
 }
 
-$TASKLIST = (Get-ScheduledTask BT_BAN_*).TaskName
-if ($TASKLIST) {
-	echo ""
-	echo "  清除以下任务计划"
-	echo ""
-	$TASKLIST | ForEach-Object {'  ' + $_}
-	echo ""
+if ($TASKLIST = Get-ScheduledTask BT_BAN_*) {
+	Write-Host "`n  清除以下任务计划`n"
+	$TASKLIST.TaskName | ForEach-Object {'  ' + $_}
+	Write-Host
 	pause
-	Unregister-ScheduledTask $TASKLIST -Confirm:$false
+	Unregister-ScheduledTask $TASKLIST.TaskName -Confirm:$false
 } else {
-	echo ""
-	echo "  没有需要清除的任务计划"
+	Write-Host "`n  没有需要清除的任务计划`n"
 }
 
-$DYKWID = '{3817fa89-3f21-49ca-a4a4-80541ddf7465}'
-$DYKWNAME = (Get-NetFirewallDynamicKeywordAddress -Id $DYKWID -ErrorAction Ignore).Keyword
-if ($DYKWNAME) {
-	echo ""
-	echo "  清除以下动态关键字"
-	echo ""
-	$DYKWNAME | ForEach-Object {'  ' + $_}
-	echo ""
+$GUID = '{3817fa89-3f21-49ca-a4a4-80541ddf7465}'
+if ($DYKW = Get-NetFirewallDynamicKeywordAddress -Id $GUID -ErrorAction Ignore) {
+	Write-Host "`n  清除以下动态关键字`n"
+	$DYKW.Keyword | ForEach-Object {'  ' + $_}
+	Write-Host
 	pause
-	Remove-NetFirewallDynamicKeywordAddress -Id $DYKWID
+	Remove-NetFirewallDynamicKeywordAddress -Id $GUID
 } else {
-	echo ""
-	echo "  没有需要清除的动态关键字"
+	Write-Host "`n  没有需要清除的动态关键字`n"
 }
 
-$FILELIST = (Get-Childitem $env:USERPROFILE\BT_BAN -Recurse).FullName
-if ($FILELIST) {
-	echo ""
-	echo "  清除以下脚本文件"
-	echo ""
-	echo "  $env:USERPROFILE\BT_BAN"
-	$FILELIST | ForEach-Object {'  ' + $_}
-	echo ""
+if (Test-Path $ENV:USERPROFILE\BT_BAN) {
+	Write-Host "`n  清除以下脚本文件`n"
+	Write-Host "  $ENV:USERPROFILE\BT_BAN"
+	(Get-Childitem $ENV:USERPROFILE\BT_BAN -Recurse).FullName | ForEach-Object {'  ' + $_}
+	Write-Host
 	pause
-	Remove-Item $env:USERPROFILE\BT_BAN -Force -Recurse -ErrorAction Ignore
+	Remove-Item $ENV:USERPROFILE\BT_BAN -Force -Recurse -ErrorAction Ignore
 } else {
-	echo ""
-	echo "  没有需要清除的脚本文件"
+	Write-Host "`n  没有需要清除的脚本文件`n"
 }
 
-echo ""
-echo "  已清除所有配置"
-echo ""
+Write-Host "`n  已清除所有配置`n"
