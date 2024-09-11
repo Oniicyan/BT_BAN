c1459c51f00ad65643aa44152a5c5f20e606ce42
Oniicyan <130678414+Oniicyan@users.noreply.github.com>
2024-09-11 12:40:38 +0800

    Create Re_enable_Notify.ps1

---
+++ Re_enable_Notify.ps1
@@ -0,0 +1,28 @@
+$AppId = 'BT_BAN_IPLIST'
+Remove-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings\$AppId" -Name "Enabled" -Force -ErrorAction Ignore
+"" | Out-File -Force $ENV:LOCALAPPDATA\Microsoft\Windows\Notifications\wpndatabase.db
+"" | Out-File -Force $ENV:LOCALAPPDATA\Microsoft\Windows\Notifications\wpndatabase.db-wal
+Write-Host "`n  正在重启通知平台，可能需要几分钟...`n"
+$SVCLIST = Get-Service -Name OneSyncSvc_*,CDPUserSvc_*,WpnUserService_*
+foreach ($SVCNAME in $SVCLIST.Name) {
+	Start-Job {Stop-Service $Using:SVCNAME} | Out-Null
+}
+Get-Job | Wait-Job | Out-Null
+try {
+	Remove-Item $ENV:LOCALAPPDATA\Microsoft\Windows\Notifications\wpndatabase.db-shm -Force -ErrorAction Ignore
+} catch {}
+foreach ($SVCNAME in $SVCLIST.Name) {
+	Start-Job {Start-Service $Using:SVCNAME} | Out-Null
+}
+Get-Job | Wait-Job | Out-Null
+Get-Job | Remove-Job -Force
+$XML = '<toast><visual><binding template="ToastText01"><text id="1">已恢复推送通知</text></binding></visual></toast>'
+$XmlDocument = [Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime]::New()
+$XmlDocument.loadXml($XML)
+[Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime]::CreateToastNotifier($AppId).Show($XmlDocument)
+Write-Host "  已恢复推送通知`n"
+if ((Fltmc).Count -eq 3) {
+	Write-Host "  如未显示，请以管理员权限执行`n"
+} else {
+	Write-Host "  如未显示，请重启 Windows`n"
+}
