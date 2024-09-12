5338a357f447222fdcab2ec6d0bed704fce6c965
Oniicyan <130678414+Oniicyan@users.noreply.github.com>
2024-09-12 10:13:26 +0800

    Update TEST.ps1

--- TEST.ps1
+++ TEST.ps1
@@ -8,6 +8,6 @@ if ((Fltmc).Count -eq 3) {
 	Write-Host "`n  以管理员权限重新执行`n"
 	Invoke-Expression "Start-Process $PROCESS -Verb RunAs"
 } else {
-	Write-Host "`n  OK`n"
+	$MyInvocation.MyCommand.Definition
 }
 pause
