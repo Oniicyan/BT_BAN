9b1fe468a8bb3bd3d7078d6bb583fedec848fb40
Oniicyan <130678414+Oniicyan@users.noreply.github.com>
2024-09-12 09:35:45 +0800

    Update TEST.ps1

--- TEST.ps1
+++ TEST.ps1
@@ -1,6 +1,12 @@
-$MyInvocation
+$WTPATH = "$ENV:LOCALAPPDATA\Microsoft\WindowsApps\wt.exe"
+if (Test-Path $WTPATH) {
+	$SHELL = "$WTPATH powershell"
+} else {
+	$SHELL = "powershell"
+}
 if ((Fltmc).Count -eq 3) {
 	Write-Host "`n  以管理员权限重新执行`n"
+	Start-Process $SHELL -ArgumentList $MyInvocation.MyCommand
 } else {
 	Write-Host "`n  OK`n"
 }
