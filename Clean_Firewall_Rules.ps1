47dba4a7c845c78344665dec3ab365640d692512
Oniicyan <130678414+Oniicyan@users.noreply.github.com>
2024-09-12 17:14:08 +0800

    Update Clean_Firewall_Rules.ps1

--- Clean_Firewall_Rules.ps1
+++ Clean_Firewall_Rules.ps1
@@ -41,8 +41,7 @@ if ($LOST) {
 	return
 }
 
-Write-Host
-Write-Host 以上过滤规则的关联程序已被删除或移动
+Write-Host `n以上过滤规则的关联程序已被删除或移动
 Write-Host Inbound/Outbound 代表 入站规则/出站规则`n
 Write-Host 如要清理，请按 Enter 键
 Write-Host 如要退出，请按 Ctrl+C 键或关闭本窗口`n
