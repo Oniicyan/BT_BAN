8a7279d08bd5f3064b31d96b2312328da66d3671
Oniicyan <130678414+Oniicyan@users.noreply.github.com>
2024-12-25 10:35:08 +0800

    Update BT_BAN_EXTRA.ps1

--- BT_BAN_EXTRA.ps1
+++ BT_BAN_EXTRA.ps1
@@ -15,7 +15,7 @@ Write-Host "  留空则清除附加规则"
 $EXTURL = Read-Host `n规则地址
 
 if ($EXTURL) {
-	try {$EXLIST = $(Invoke-RestMethod $EXTURL -TimeoutSec 30)
+	try {$EXTEXT = $(Invoke-RestMethod $EXTURL -TimeoutSec 30)
 	} catch {
 		Write-Host "`n  获取附加规则失败，请检查网络或本地文件地址"
 		Read-Host `n操作失败，按 Enter 键结束...
@@ -23,8 +23,8 @@ if ($EXTURL) {
 	}
 }
 
-$EXLIST = [Regex]::Matches($EXTEXT,'((\d{1,3}\.){3}\d{1,3}(\/\b([1-9]|[12][0-9]|3[0-2])\b)?)|(([0-9a-f]{1,4}::?){1,7}(([0-9a-f]{1,4})|:)(\/\b([1-9]|[1-9][0-9]|1[01][0-9]|12[0-8])\b)?)').Value
-if ($EXLIST) {
+$EXLIST = [Regex]::Matches($EXTEXT,'((\d{1,3}\.){3}\d{1,3}(\/\b([1-9]|[12][0-9]|3[0-2])\b)?)|([0-9a-f]{4}:([0-9a-f]{1,4}::?){1,6}(([0-9a-f]{1,4})|:)(\/\b([1-9]|[1-9][0-9]|1[01][0-9]|12[0-8])\b)?)').Value
+if (!$EXLIST) {
 	Write-Host "`n  解析附加规则失败，请确认 IP 格式"
 	Read-Host `n操作失败，按 Enter 键结束...
 	return
